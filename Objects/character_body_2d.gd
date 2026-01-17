d0eac68d5df942146dd79a54a25d6d33526ef4a3
MrPoWasTaken <jerrygw2004@gmail.com>
2026-01-16 21:19:14 -0500

    Enemy Healthbars working

--- Objects/character_body_2d.gd
+++ Objects/character_body_2d.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
-var Hitpoints = 100
+@export var _healthBar: Node
+
 
 func _ready() -> void:
 	pass
 
 func takeDamage(damage: float) -> void:
-	print("Hitpoints before %s" %Hitpoints)
-	Hitpoints -= damage
-	print("Hitpoints after %s" %Hitpoints)
+	print("Hitpoints before %s" %_healthBar.value)
+	_healthBar.value -= damage
+	print("Hitpoints after %s" %_healthBar.value)
