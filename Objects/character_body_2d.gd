961f8911f9a4182247c6473b5fe307c45323fed6
MrPoWasTaken <jerrygw2004@gmail.com>
2026-01-17 21:13:29 -0500

    Broken bar reference and moving enemy health bar to player node

--- Objects/character_body_2d.gd
+++ Objects/character_body_2d.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
-@export var _healthBar: Node
-
+@onready var _healthBar = get_tree().get_nodes_in_group("PlayerHurtBox")
+const ATTACKS = ["spawns", "tentacles"]
 
 func _ready() -> void:
 	pass
 
 func takeDamage(damage: float) -> void:
-	print("Hitpoints before %s" %_healthBar.value)
-	_healthBar.value -= damage
-	print("Hitpoints after %s" %_healthBar.value)
+	print(_healthBar)
+	#print("Hitpoints before %s" %_healthBar.value)
+	#_healthBar.value -= damage
+	#print("Hitpoints after %s" %_healthBar.value)
